f7163a7123094aac96541a29404ed94410f2466d
Milan2014270174 <milandjordjevic2707@gmail.com>
2022-06-25 23:10:54 +0200

    placena rezervacija fix

--- frontend/src/components/modals/ReservationModal/ReservationModal.tsx
+++ frontend/src/components/modals/ReservationModal/ReservationModal.tsx
@@ -56,7 +56,8 @@ const ReservationModal = ({
           ticket_type: ticket_type,
           to_bus_line_station_id: to_bus_line_station_id,
           from_bus_line_station_id: from_bus_line_station_id,
-          number_of_tickets: input.ticketNumber
+          number_of_tickets: input.ticketNumber,
+          is_paid: input.is_paid
         })
         .then((res) => {
           console.log(res.data)
